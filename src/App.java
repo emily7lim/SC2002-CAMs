92fe805b429d569388db27e859c52b77052ec376
Choh0004 <144433388+Choh0004@users.noreply.github.com>
2023-11-10 23:27:43 +0800

    Add files via upload

--- src/App.java
+++ src/App.java
@@ -6,7 +6,8 @@ import database.Database;
 import model.Camp;
 import model.enums.Faculty;
 import report.ReportController;
-import report.ReportType;
+import report.enums.ReportType;
+import report.enums.ReportOutputType;
 
 public class App {
     public static void main(String[] args) {
@@ -19,11 +20,19 @@ public class App {
     }
 
     private static void writeReportExample(){
-        Camp exampleCamp = new Camp("test camp", new Date(), new Date(1699012689503L), new Date(1699011699503L), Faculty.ADM, Faculty.ADM.getFaculty(), 10, 3, "test camp", "OURIN");
         List<Camp> camps = new ArrayList<Camp>();
-        camps.add(exampleCamp);
+        Camp exampleCamp1 = new Camp("test' \"camp", new Date(), new Date(1699012689503L), new Date(1699011699503L), Faculty.ADM, Faculty.ADM.getFaculty(), 10, 3, "test camp", "OURIN");
+        camps.add(exampleCamp1);
+        Camp exampleCamp2 = new Camp("test camp2", new Date(), new Date(1699012689503L), new Date(1699011699503L), Faculty.ADM, Faculty.ADM.getFaculty(), 15, 3, "test camp2", "OURIN");
+        camps.add(exampleCamp2);
+        
+        // test string cos formatting might mess up 
+        String testCsv = "\"Field with a comma, inside quotes\",\"Field with \"double\" quotes\"," +
+                "\"Field with line break or newline\",\"Field with tab\tspace\",\"1234560001\"";
+        Camp exampleCamp3 = new Camp(testCsv, new Date(), new Date(1699012689503L), new Date(1699011699503L), Faculty.ADM, Faculty.ADM.getFaculty(), 15, 3, "to test formatting", "OURIN");
+        camps.add(exampleCamp3);
 
-        ReportController rp = new ReportController(ReportType.TXT);
-        rp.generateAndWriteReports(camps);
+        ReportController rp = new ReportController(ReportOutputType.CSV);
+        rp.generateAndWriteReports(camps, ReportType.CAMP_LIST);
     }
 }
