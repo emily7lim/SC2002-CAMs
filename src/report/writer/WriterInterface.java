099d2fb5058494705ed6a51eb1e870ed8d458d97
Choh0004 <144433388+Choh0004@users.noreply.github.com>
2023-11-15 09:41:55 +0800

    Add files via upload

--- src/report/writer/WriterInterface.java
+++ src/report/writer/WriterInterface.java
@@ -4,14 +4,16 @@ package report.writer;
  * Defines methods for classes that write reports to files.
  * Implementing classes should provide methods to write report data to specified files.
  * 
+ * @author Choh Lit Han Owen
+ * @version 1.0
  * @since 2023-11-03
  */
 public interface WriterInterface {
     /**
      * Writes report data to a specified file.
      * 
-     * @param csvData The formatted report data to be written to the file.
+     * @param content The formatted report data to be written to the file.
      * @param filePath The path to the file where the report data will be saved.
      */
-    public static void writeReportToFile(String csvData, String filePath) {};
+    public static void writeReportToFile(String content, String filePath) {};
 }
