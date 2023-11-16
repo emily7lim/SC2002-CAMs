3fca21746083ddfbadebffed7bac0a7e8e334f2f
Choh0004 <144433388+Choh0004@users.noreply.github.com>
2023-11-16 12:53:55 +0800

    to fix camp issue

--- src/database/DatabaseMenu.java
+++ src/database/DatabaseMenu.java
@@ -1,23 +1,54 @@
 package database;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import model.*;
 import model.enums.Faculty;
 // just for us to use to set the .dat
 public class DatabaseMenu {
-    private static HashMap<String, User> dbTable;
+    private static HashMap<String, Camp> dbTable;
     public static void main(String[] args) {
         new Database();
-        dbTable = Database.USERS;
+        dbTable = Database.CAMPS;
         
+        dbTable.forEach((key, camp) -> {
+            // Modify the value (e.g., double it)
+            if(camp.getWithdrawnParticipantIds() == null) camp.setWithdrawnParticipantIds(new ArrayList<>());
+            if(camp.getParticipantIds() == null) camp.setParticipantIds(new ArrayList<>());
+            if(camp.getCommitteeIds() == null) camp.setCommitteeIds(new ArrayList<>());
+            if(camp.getEnquiryIds() == null) camp.setEnquiryIds(new ArrayList<>());
+            if(camp.getSuggestionIds() == null) camp.setSuggestionIds(new ArrayList<>());
+
+            dbTable.put(key, camp);
+        });
+        
+        Database.saveAllToDatabase();
+    }
+    public static void printdb(){
+        DatabaseMenu.dbTable.forEach((userId, user) -> {
+            System.out.println("UserID: " + userId + ", Name: " + user.getName());
+        });
+    }
+    private static String getUID(String email){
+        int end = email.indexOf("@");
+        if(end == -1){
+            return email;
+        } else {
+            return email.substring(0, end);
+        }
+    }
+    private void updateCamp(){
+
+    }
+    /*
+    private void something(){
         Scanner sc = new Scanner(System.in);
 
         String[] choices = new String[3];
         String choice = "";
         int count = 0;
-        
         while(!choice.equals("-exit")){
             System.out.println("choice: ");
             choice = sc.next();
@@ -42,21 +73,6 @@ public class DatabaseMenu {
                 dbTable.put(staff.getUserId(), staff);
             }
         }
-
-        sc.close();
-        Database.saveAllToDatabase();
     }
-    public static void printdb(){
-        DatabaseMenu.dbTable.forEach((userId, user) -> {
-            System.out.println("UserID: " + userId + ", Name: " + user.getName());
-        });
-    }
-    private static String getUID(String email){
-        int end = email.indexOf("@");
-        if(end == -1){
-            return email;
-        } else {
-            return email.substring(0, end);
-        }
-    }
+    */
 }
